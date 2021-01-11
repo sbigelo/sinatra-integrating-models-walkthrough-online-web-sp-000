0dcf0c72049326fcc48eec456b9f9ebf93a7793f
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-11 21:08:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,8 +4,7 @@ require_relative 'models/text_analyzer.rb'
 class App < Sinatra::Base
 
   get '/' do
-    text_from_user = params[:user_text]
-    @analyzed_text = TextAnalyzer.new(text_from_user)
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
     erb :index
   end
 
